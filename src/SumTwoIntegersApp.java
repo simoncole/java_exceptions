bc288b49a92e59045022106f74dd2188983f3d92
Simon Cole <simonccole1@gmail.com>
2023-09-21 13:54:58 -0400

    implemented try-catch

---
+++ src/SumTwoIntegersApp.java
@@ -0,0 +1,28 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class SumTwoIntegersApp {
+    public static void main(String[] args) throws Exception {
+
+        int[] nums = getInputs();
+        System.out.println("The sum is: " + (nums[0] + nums[1]));
+
+    }
+    public static int[] getInputs(){
+        try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter first number: ");
+                int int1 = scanner.nextInt();
+                System.out.print("Enter second number: ");
+                int int2 = scanner.nextInt();
+                int[] nums = {int1, int2};
+                scanner.close();
+
+                return nums;
+            }
+            catch(InputMismatchException e){
+                System.out.println("Wrong input type, try again");
+                return getInputs();
+            }
+    }
+}
